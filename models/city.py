8a04c3ab3572a523356859e70d3751f883b7f5ce
Boukind Rachid <boukind.r1@gmail.com>
2024-06-26 05:40:41 -0700

    a

--- models/city.py
+++ models/city.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""City class."""
-from models.base_model import BaseModel
+"""This is the city class"""
+from models.base_model import BaseModel, Base, Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ Initialize city instance."""
+class City(BaseModel, Base):
+    """This is the class for City"""
 
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state = relationship('State', back_populates='cities')
