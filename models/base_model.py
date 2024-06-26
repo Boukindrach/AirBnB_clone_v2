87f3a82fa5b7e41372e8588dc72cebb597acd5d4
Boukind Rachid <boukind.r1@gmail.com>
2024-06-26 05:38:30 -0700

    a

--- models/base_model.py
+++ models/base_model.py
@@ -20,14 +20,26 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize BaseModel instance."""
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, time_format)
-                else:
-                    self.__dict__[k] = v
+        if kwargs and "id" not in self.__dict__:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
         else:
-            models.storage.new(self)
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def save(self):
         """Save the instance to storage."""
