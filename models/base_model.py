2b32073f599eab7cbe942fc6b08d516c0a664b04
Boukind Rachid <boukind.r1@gmail.com>
2024-06-26 12:40:12 -0700

    a

--- models/base_model.py
+++ models/base_model.py
@@ -10,18 +10,17 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize BaseModel instance."""
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        from models import storage
         self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        storage.new(self)
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, time_format)
-                else:
-                    self.__dict__[k] = v
-        else:
-            models.storage.new(self)
+                    v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                    if k != '__class__' and hasattr(self.__class__, k):
+                        setattr(self, k, v)
 
     def save(self):
         """Save the instance to storage."""
