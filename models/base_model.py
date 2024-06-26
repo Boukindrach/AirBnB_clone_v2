1aae048f626179cac060a4d166be5f1b98a58f7a
Boukind Rachid <boukind.r1@gmail.com>
2024-06-26 05:36:13 -0700

    a

--- models/base_model.py
+++ models/base_model.py
@@ -20,9 +20,6 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize BaseModel instance."""
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
