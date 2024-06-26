183f44a20a0c7878181caa3cfbe342e7e221aa99
Boukind Rachid <boukind.r1@gmail.com>
2024-06-26 13:01:32 -0700

    a

--- models/state.py
+++ models/state.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """State class."""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+import os
 
 
 class State(BaseModel):
