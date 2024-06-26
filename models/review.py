fd59676477b651abef27971ef7cb740785454be4
Boukind Rachid <boukind.r1@gmail.com>
2024-06-26 05:29:51 -0700

    a

--- models/review.py
+++ models/review.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""Review class."""
-from models.base_model import BaseModel, Base, Column, String
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
+""" Review module for the HBNB project """
+from models.base_model import BaseModel
 
-class Review(BaseModel, Base):
-    """Initialize Review instance."""
 
-    __tablename__ = 'reviews'
-
-    text = Column(String(1024), nullable = False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable = False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable = False)
-
-    user = relationship('User', back_populates = 'reviews')
-    place = relationship('Place', back_populates = 'reviews')
+class Review(BaseModel):
+    """ Review classto store review information """
+    place_id = ""
+    user_id = ""
+    text = ""
--- models/user.py
+++ models/user.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""User class."""
-from models.base_model import BaseModel, Base, Column, String
-from sqlalchemy.orm import relationship
+"""This module defines a class User"""
+from models.base_model import BaseModel
 
-class User(BaseModeln Base):
-    """Initialize User instance."""
 
-    __tablename__ = 'users'
-
-    email = Column(String(128), nullable = False)
-    password = Column(String(128), nullable = False)
-    first_name = Column(String(128), nullable = True)
-    last_name = Column(String(128), nullable = True)
-
-    places = relationship("Place", back_populates = "user", cascade = "delete")
-    reviews = relationship("Review", back_populates = "user", cascade = "delete")
+class User(BaseModel):
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
