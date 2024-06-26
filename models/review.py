cf5869c16f65a696ac7158a2495c5d893d99f613
Boukind Rachid <boukind.r1@gmail.com>
2024-06-26 14:12:18 -0700

    a

--- models/review.py
+++ models/review.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """Review class."""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 
 class Review(BaseModel):
     """Initialize Review instance."""
 
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    user = relationship('User', back_populates='reviews')
+    place = relationship('Place', back_populates='reviews')
