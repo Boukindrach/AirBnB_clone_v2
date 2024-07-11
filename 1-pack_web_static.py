b5cfedf20500f5887c2d2f47be39e98b58a75b34
Boukind Rachid <boukind.r1@gmail.com>
2024-07-11 13:13:37 -0700

    a

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-#  Fabric script that generates a .tgz archive
-# from the contents of the web_static folder of 
-# your AirBnB Clone repo, using the function do_pack.
+# Fabric script that generates a .tgz archive.
 import os
 from datetime import datetime
 from fabric.api import local
